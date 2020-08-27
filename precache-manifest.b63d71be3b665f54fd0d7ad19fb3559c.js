7166159128f2626bd8cc525a72f5ee9135083f73
sukritincs <sukritiverma@northcorp.tech>
2020-08-28 01:59:56 +0530

    Updates

---
+++ precache-manifest.b63d71be3b665f54fd0d7ad19fb3559c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca093a517bf55e5d3c0c7c644272b020",
+    "url": "/Admin-boilerplate/index.html"
+  },
+  {
+    "revision": "2c4583841e665d5e5eea",
+    "url": "/Admin-boilerplate/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "760587007d3dcd8fac37",
+    "url": "/Admin-boilerplate/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "2c4583841e665d5e5eea",
+    "url": "/Admin-boilerplate/static/js/2.768070c9.chunk.js"
+  },
+  {
+    "revision": "ea717a1fbb69e218c1cb649e92f0d3a8",
+    "url": "/Admin-boilerplate/static/js/2.768070c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760587007d3dcd8fac37",
+    "url": "/Admin-boilerplate/static/js/main.b747916b.chunk.js"
+  },
+  {
+    "revision": "6c14d5eaadcfece80dd7",
+    "url": "/Admin-boilerplate/static/js/runtime-main.2289710f.js"
+  }
+]);
