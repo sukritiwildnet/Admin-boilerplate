267116f2bff1c8aa3b361b5752423909940d26d0
sukritincs <sukritiverma@northcorp.tech>
2020-08-28 01:29:16 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev--51c0mci.us.auth0.com"
+    clientId="FxvHp6bnYPI8G2yzlMh5BtWQpa64bh2w"
+    redirectUri="http://localhost:3000/dashboard">
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
