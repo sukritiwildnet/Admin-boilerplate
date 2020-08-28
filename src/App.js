460ba51a256f1dca235e992cb76ddb2565385b79
sukritincs <sukritiverma@northcorp.tech>
2020-08-28 11:55:03 +0530

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Chart } from 'react-chartjs-2';
 import { ThemeProvider } from '@material-ui/styles';
@@ -11,6 +11,8 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
 import validators from './common/validators';
 import Routes from './Routes';
+import Dashboard from './views/Dashboard';
+import SignInView from './views/SignIn';
 
 const browserHistory = createBrowserHistory();
 
@@ -28,7 +30,12 @@ export default class App extends Component {
     return (
       <ThemeProvider theme={theme}>
         <Router history={browserHistory}>
-          <Routes />
+          <Route
+            component={SignInView}
+            exact
+            // layout={MinimalLayout}
+            path="/sign-in"
+          />
         </Router>
       </ThemeProvider>
     );
